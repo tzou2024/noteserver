52c0a728b6d07a0e8388ee38f57584e1ea1788cb
Trevor Zou <tzou@olin.edu>
2022-08-26 15:19:24 -0500

    prepare deployment

--- notes/settings.py
+++ notes/settings.py
@@ -80,6 +80,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
